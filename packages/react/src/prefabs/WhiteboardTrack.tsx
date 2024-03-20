d9dcce2b49a0cbd25969cb67ceb531c3edfc01b3
Apurv Bhavsar <apurvsb@teamlocus.net>
2024-03-20 12:54:41 +0530

    add username with whiteboard url

--- packages/react/src/prefabs/WhiteboardTrack.tsx
+++ packages/react/src/prefabs/WhiteboardTrack.tsx
@@ -15,8 +15,19 @@ import { useRoomContext } from '../context';
  * @public
  */
 export function WhiteboardTrack() {
-    const room = useRoomContext();
-    const url = `https://cloud13.de/testwhiteboard/?whiteboardid=${room.name}`;
+    return <iframe src={getURL()} width={"100%"} height={'100%'}></iframe>;
+}
 
-    return <iframe src={url} width={"100%"} height={'100%'}></iframe>;
+export function getURL() {
+    const room = useRoomContext();
+
+    let url = `https://cloud13.de/testwhiteboard/?whiteboardid=${room.name}`;
+
+    const username = room.localParticipant.name;
+
+    if (username) {
+        url += `&username=${username}`;
+    }
+
+    return url;
 }
