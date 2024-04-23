61128c0115380c4a34d5b6924a22acdaf6cd0adf
Apurv Bhavsar <apurvsb@teamlocus.net>
2024-04-23 15:30:16 +0530

    update

--- packages/react/src/prefabs/ControlBar.tsx
+++ packages/react/src/prefabs/ControlBar.tsx
@@ -219,10 +219,10 @@ export function ControlBar({
       )}
       {visibleControls.endForAll ? (
         <div className="tl-leave lk-button-group">
-          <button className="lk-disconnect-button">
+          {/* <button className="lk-disconnect-button">
             {showIcon && <LeaveIcon />}
             {showText && "Leave"}
-          </button>
+          </button> */}
 
           <div className="tl-leave-btn lk-button-group-menu">
             <HostEndMeetingMenu
